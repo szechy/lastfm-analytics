addc2b16775f6686714f69693de72f473b19ac94
szechy <cszechy@umich.edu>
2015-01-26 18:46:13 -0500

    Added Unix timestamp time functionality

--- retrieve-last-month.py
+++ retrieve-last-month.py
@@ -1,4 +1,5 @@
 import pylast
+import time
 
 #	Unique application values
 keys = open('lastfm-info', 'r')
@@ -12,16 +13,25 @@ API_SECRET = TEMP_API_SECRET.replace("\n", "")
 #	To perform a write operation, need to auth myself
 username = "kenavt"
 #	I think the fact I have to provide my password is ridiculous
-password = pylast.md5(keys.readline())
+#password = pylast.md5(keys.readline())
+temp_pass = keys.readline()
+password = pylast.md5(temp_pass.replace("\n", ""))
 
 network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET)
 user = network.get_user(username)
 
 # retrieve last month's songs
-#songs = user.get_recent_tracks();
-n = 1
+n = 2
+time = 31	# as in, 31 days
 
 while True:
-	tracks = user.get_recent_tracks(n)
-	print tracks[0].playback_date
-	print type(tracks[0].playback_date)
+tracks = user.get_recent_tracks(n)
+	# two options to retrieve time
+	# we'll use the timestamp method, but strptime() method works
+	# date = time.strptime(tracks[-1].playback_date, "%d %b %Y, %H:%M")
+	timestamp = time.gmtime(float(tracks[-1].timestamp))
+	print date.tm_year
+	print timestamp.tm_year
+	print date.tm_mon
+	print timestamp.tm_mon
+	print date.tm_mday
